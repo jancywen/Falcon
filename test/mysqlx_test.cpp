62c0eadb8b9d8eb0ad63efb6e8e0a9c370e8aba6
wangwenjie <jancywen@163.com>
2020-09-19 16:57:45 +0800

    recharge、 borrow book、return book

--- test/mysqlx_test.cpp
+++ test/mysqlx_test.cpp
@@ -34,24 +34,23 @@ void insertReaderData(std::string card_id,
                       int age, 
                       std::string tel, 
                       float balance);
-// 借阅
-void insertBorrowData(int book_id, std::string card_id);
+
+// 充值
+void recharge(Session &session, std::string card_id, float balance);
+// 借书
+void borrowBook(Session &session, int book_id, std::string card_id);
+// 还书
+void returnBook(Session &session, int book_id, std::string card_id);
+
 
 // 查询 类别图书
-void searchBookRecord(std::string category_name);
-
+void searchBookRecord(Session &session, std::string category_name); 
 // 查询所有借阅信息
 void queryAllBorrowRecord();
 // 查询图书借阅信息 
 void searchBorrowRecord(std::string book_name);
 // 查询未归还的借阅信息
 void queryBorrowRecord();
-
-// 还书
-void returnBook(int book_id, std::string card_id);
-
-// 充值
-void recharge(std::string card_id, float balance);
 
 
 int main()
@@ -82,38 +81,21 @@ int main()
     insertReaderData("4562135471", "赵子龙", "男", 17, "12739247", 200);
     insertReaderData("4562135472", "典韦", "男", 25, "12739247", 200);
 
-    // 插入 借阅信息
-    insertBorrowData(20200913, "4562135468");
-    insertBorrowData(20200915, "4562135465");
-    insertBorrowData(20200911, "4562135472");
 */
 
-    // searchBookRecord("机械工程");
 
     Session session(HOST, PORT, USER, PWD);
 
-try {
-        session.sql("use book_schema").bind(SCHEMA).execute();
-        SqlResult result = session.sql("select category from bookcategory where category_id = ?;").bind(6).execute();
-        // SqlResult result = session.sql("select book_id, book_name, category, author from bookinfo inner join bookcategory on bookinfo.book_category_id = bookcategory.category_id where (book_category_id = select category_id from bookcategory where category = 软件工程)").execute() ;
-        list<Row> list  = result.fetchAll();
-    
-        int column_count = result.getColumnCount();
+    // 充值
+    // recharge(session, "4562135470", 50);
 
-        // const Columns &columns = result.getColumns();
-        
-        for (Row row : list) 
-        {
-            for (int i = 0; i < column_count; i++)
-            {
-                cout << row[i] << " ";
-            }
-            cout << endl;
-        }
-    }catch (exception &e) {
-        cout << e.what() <<endl;
-    }
+    // 借书
+    // borrowBook(session, 20200911, "4562135471");
 
+    // 还书
+    // returnBook(session, 20200913, "4562135468");
+
+    session.close();
     return 0;
 }
 
@@ -239,13 +221,19 @@ void insertBorrowData(int book_id, std::string card_id)
 
 
 // 查询 类别 图书
-void searchBookRecord(std::string category_name)
+void searchBookRecord(Session &session, std::string category_name)
 {
-    Session session(HOST, PORT, USER, PWD);
-    
     try {
-        session.sql("use book_schema").bind(SCHEMA).execute();
-        SqlResult result = session.sql("select book_id, book_name, category, author from bookinfo inner join bookcategory on bookinfo.book_category_id = bookcategory.category_id where (book_category_id = select category_id from bookcategory where category = ?)").bind(category_name).execute() ;
+        session.sql("use " + std::string(SCHEMA)).execute();
+        SqlResult result = session.sql("select book_id, book_name, category, author" 
+                                       " from bookinfo" 
+                                       " inner join bookcategory" 
+                                       " on bookinfo.book_category_id = bookcategory.category_id" 
+                                       " where book_category_id =" 
+                                       " (select category_id from bookcategory where category = ?)")
+                                  .bind(category_name)
+                                  .execute() ;
+
         list<Row> list  = result.fetchAll();
     
         int column_count = result.getColumnCount();
@@ -263,12 +251,42 @@ void searchBookRecord(std::string category_name)
     }catch (exception &e) {
         cout << e.what() <<endl;
     }
-
-    session.close();
-
 }
 
-// 查询所有借阅信息
+// 创建 费用表
+void calculateCost(Session & session) 
+{
+    std::string quoted_name = std::string("`") + std::string(SCHEMA) + "`.`" + "readerfee" + "`";
+
+    std::string create = "CREATE TABLE ";
+    create += "IF NOT EXISTS";
+    create += quoted_name;
+    create += "(book_id int, card_id char(18), return_date date, actual_return_date date, book_fee decimal(7, 3));";
+
+    session.sql(create).execute();
+
+    // 插入数据
+    session.sql("use " + std::string(SCHEMA)).execute();
+    session.sql("insert into readerfee(book_id, card_id, return_date)" 
+                " select book_id, card_id, return_date" 
+                " from borrowinfo" " where datediff(sysdate(), return_date)>0" 
+                " and status = '否';")
+           .execute();
+
+    SqlResult res = session.sql("select * from readerfee").execute();
+    list<Row> list = res.fetchAll();
+    int column_count = res.getColumnCount();
+    for (Row row : list) 
+        {
+            for (int i = 0; i < column_count; i++)
+            {
+                cout << row[i] << " ";
+            }
+            cout << endl;
+        }
+}
+
+// 查询某段时间所有借阅信息  xx 于 xx 借阅了 xxx，xx归还，费用：xx 或者 未归还
 void queryAllBorrowRecord()
 {
 
@@ -284,14 +302,105 @@ void queryBorrowRecord()
 
 }
 
-// 还书
-void returnBook(int book_id, std::string card_id)
+// 充值
+void recharge(Session &session, std::string card_id, float balance)
 {
-
+    session.startTransaction();
+    try
+    {
+        session.sql("use " + std::string(SCHEMA)).execute();
+        session.sql("SET @bal = ?").bind(balance).execute();
+        session.sql("SET @cardid = ?").bind(card_id).execute();
+        session.sql("update readerinfo" 
+                    " set balance = balance + @bal" 
+                    " where card_id = @cardid").execute();
+        session.commit();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        cout << e.what() <<endl;
+        session.rollback();
+    }
 }
 
-// 充值
-void recharge(std::string card_id, float balance)
-{
+// 借书
+void borrowBook(Session &session, int book_id, std::string card_id)
+{   
+    // 查询是否有未还的书
+    session.sql("use " + std::string(SCHEMA)).execute();
+    session.sql("SET @bookid = ?").bind(book_id).execute();
+    session.sql("SET @cardid = ?").bind(card_id).execute();
+    session.sql("SET @borrowinfo = ?").bind(BORROW_INFO).execute();
+    SqlResult res = session.sql("select * from borrowinfo"  
+                                " where book_id = @bookid" 
+                                " and card_id = @cardid"
+                                " and status = '否'")
+                            .execute();
+    if (!res.fetchOne())
+    {
+        session.startTransaction();
+        try 
+        {
+            session.sql("insert into borrowinfo(book_id, card_id, borrow_date, return_date)"
+                        " values(@bookid, @cardid, sysdate(), date_add(now(), interval 2 month))")
+                   .execute();
+            session.commit();
+            cout << "借书成功！！！"<<endl;
+        }
+        catch (exception &e)
+        {
+            cout <<e.what() << endl;
+            session.rollback();
+            cout << "借书失败" << endl;
+        }
+    }
+    else 
+    {
+        cout << "还有书没有还" <<endl;
+    }
+}
 
+// 还书
+void returnBook(Session &session, int book_id, std::string card_id)
+{
+    // 查询借阅记录
+    Schema schema = session.getSchema(SCHEMA);
+    Table table = schema.getTable(BORROW_INFO);
+    RowResult res = table.select("return_date").where("book_id = :bookid and card_id = :cardId and status = '否'")
+                  .bind("bookid", book_id)
+                  .bind("cardId", card_id)
+                  .execute();
+    if (!res.fetchOne())
+    {
+        cout << "没有此书的借阅信息" << endl;
+        return;
+    }
+    cout << "开始还书" <<endl;
+
+    session.startTransaction();
+    try
+    {
+        session.sql("use " + std::string(SCHEMA)).execute();
+        session.sql("SET @bookid = ?").bind(book_id).execute();
+        session.sql("SET @cardid = ?").bind(card_id).execute();
+
+        session.sql("update borrowinfo t1 join readerinfo t2 on t1.card_id = t2.card_id" 
+                    " set actual_return_date = sysdate()," 
+                    " status = '是',"
+                    " book_fee = (case when datediff(sysdate(), return_date) > 0 then datediff(sysdate(), return_date)*0.2 else 0.0 end)," 
+                    " balance = (balance - (case when datediff(sysdate(), return_date) > 0 then datediff(sysdate(), return_date)*0.2 else 0.0 end))" 
+                    " where t1.book_id = @bookid and t1.card_id = @cardid")
+               .execute();
+        session.commit();
+    }
+    catch(const std::exception& e)
+    {
+        cout <<e.what() << endl;
+        session.rollback();
+        cout << "还书失败" << endl;
+        return;
+    }
+
+    cout << "还书成功" << endl;
 }

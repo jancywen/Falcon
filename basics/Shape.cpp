155ed9ed1e3c3391427026d0e391d2f51dd98778
wangwenjie <jancywen@163.com>
2020-09-25 16:32:25 +0800

    memory management
    RAII

--- basics/Shape.cpp
+++ basics/Shape.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
+
+enum shape_type{
+    triangle,
+    rectangle
+};
+
 class Shape
 {
     protected:
         int width, height;
     public:
         Shape(int a = 0, int b = 0);
+        virtual ~Shape(){cout << __FUNCTION__ <<endl;}
         // 纯虚函数
         virtual int area()  = 0;
         
@@ -19,7 +26,7 @@ class Rectangle: public Shape
 {   
     public:
         Rectangle(int a = 0, int b = 0):Shape(a, b){}
-
+        ~Rectangle(){cout << __FUNCTION__ <<endl;}
         int area()
         {
             cout << "Rectangle class area:" <<endl;
@@ -30,6 +37,7 @@ class Rectangle: public Shape
 class Triangle: public Shape {
     public:
         Triangle( int a = 0, int b = 0): Shape(a, b){}
+        ~Triangle(){cout << __FUNCTION__ <<endl;}
         int area()
         {
             cout << "Triangle class area:"<< endl;
@@ -37,18 +45,52 @@ class Triangle: public Shape {
         }
 };
 
+Shape * create_shape(shape_type type, int w, int h) {
+    switch (type)   
+    {
+    case triangle:
+        
+        return new Triangle(w, h);
+        break;
+    
+    case rectangle:
+        return new Rectangle(w, h);
+        break;
+    }
+}
+
+class shape_wrapper {
+    public:
+        explicit shape_wrapper(Shape *ptr = nullptr):ptr_(ptr){}
+        ~shape_wrapper()
+        {
+            cout << __FUNCTION__ << endl;
+            delete ptr_;
+        }
+        Shape * get() const {return ptr_;}
+    private:
+        Shape * ptr_;
+};
+
 int main() 
 {
-    Shape *shape;
-    Rectangle rec(10, 7);
-    Triangle tri(10, 5);
+    // Shape *shape;
+    // Rectangle rec(10, 7);
+    // Triangle tri(10, 5);
 
-    shape = &rec;
-    cout<< shape ->area() << endl;
+    // shape = &rec;
+    // cout<< shape ->area() << endl;
 
-    shape = &tri;
-    cout << shape->area() << endl;
-    clog << "clog 打印"<< endl;
+    // shape = &tri;
+    // cout << shape->area() << endl;
+    // clog << "clog 打印"<< endl;
+
+    enum shape_type type;
+    type = triangle;
+    
+    shape_wrapper ptr_wrapper(create_shape(triangle, 10, 7));
+
+    cout << ptr_wrapper.get()->area() << endl;
 
     return 0;
 }
